ba153f9a18dc19ddf34d21e11702fe34601f5bb1
George Stephenson <gf.s@hotmail.com>
2025-02-02 07:05:10 +0000

    Exercise 1.17

---
+++ chapter-1/1-17.scm
@@ -0,0 +1,13 @@
+(define (double a)
+  (+ a a))
+
+(define (halve a)
+  (/ a 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (fast-mult (double a) (halve b)))
+        (else (+ a (fast-mult a (- b 1))))))
