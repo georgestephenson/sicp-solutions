c81d8f8bdbb512c501e3cbd88b63ed56823d8e11
George Stephenson <gf.s@hotmail.com>
2025-02-05 17:24:08 +0000

    Exercise 1.40

---
+++ chapter-1/1-40.scm
@@ -0,0 +1,35 @@
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+; if x^3 + 4x^2 + 88x + 3 = 0
+; x = -0.03414
+; source: https://www.calculatorsoup.com/calculators/algebra/cubicequation.php
+
+(newtons-method (cubic 4 88 3) 1)
+;Value: -.03414344654766048
