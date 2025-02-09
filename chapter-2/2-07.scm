d43a81855fccb0813890f4efa30ee1510f2dd605
George Stephenson <gf.s@hotmail.com>
2025-02-09 00:08:44 +0000

    Exercise 2.7-2.9

---
+++ chapter-2/2-07.scm
@@ -0,0 +1,4 @@
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
---
+++ chapter-2/2-08.scm
@@ -0,0 +1,26 @@
+
+
+; Analogous to the addition of intervals and using arithmetic subtraction,
+; The lower-bound and smallest difference is made by subtracting the upper-bound of y from the lower-bound of x
+; The upper-bound or biggest difference will be subtraction lower-bound of y from the upper-bound of x
+
+(load "2-07.scm")
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (print-interval i)
+  (newline)
+  (display "(")
+  (display (lower-bound i))
+  (display ",")
+  (display (upper-bound i))
+  (display ")"))
+
+(print-interval (sub-interval (make-interval 4 6) (make-interval 1 3)))
+;(1,5)
+(print-interval (sub-interval (make-interval 5 9) (make-interval 7 13)))
+;(-8,2)
+(print-interval (sub-interval (make-interval 17 88) (make-interval 2 17)))
+;(0,86)
