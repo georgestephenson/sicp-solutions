86a9890e6ef01b9eed26a97bce9588c0f3298dab
George Stephenson <gf.s@hotmail.com>
2025-02-13 22:04:20 +0000

    Exercise 2.39

---
+++ chapter-2/2-39.scm
@@ -0,0 +1,19 @@
+(define nil '())
+
+(define (reverse sequence)
+  (fold-right 
+    (lambda (x y) (append y (list x))) 
+    nil 
+    sequence))
+
+(reverse (list 1 2 3 4))
+;Value: (4 3 2 1)
+
+(define (reverse sequence)
+  (fold-left 
+    (lambda (x y) (cons y x)) 
+    nil 
+    sequence))
+
+(reverse (list 1 2 3 4))
+;Value: (4 3 2 1)
