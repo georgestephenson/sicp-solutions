6ccc71141ddf003099025fae18d6af3b552ed895
George Stephenson <gf.s@hotmail.com>
2025-02-15 20:23:36 +0000

    Exercise 2.53 rest of solution

--- chapter-2/2-53.scm
+++ chapter-2/2-53.scm
@@ -6,3 +6,9 @@
 ;Value: ((y1 y2))
 (cadr '((x1 x2) (y1 y2)))
 ;Value: (y1 y2)
+(pair? (car '(a short list)))
+;Value: #f
+(memq 'red '((red shoes) (blue socks)))
+;Value: #f
+(memq 'red '(red shoes blue socks))
+;Value: (red shoes blue socks)
