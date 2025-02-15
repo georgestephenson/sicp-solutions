4af8f9c9c0fe57d13573406a61af911a69b3d794
George Stephenson <gf.s@hotmail.com>
2025-02-15 11:52:45 +0000

    Exercise 2.50

---
+++ chapter-2/2-50.scm
@@ -0,0 +1,34 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint (flip-horiz einstein))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(paint (rotate90 einstein))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(paint (rotate180 einstein))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint (rotate270 einstein))
