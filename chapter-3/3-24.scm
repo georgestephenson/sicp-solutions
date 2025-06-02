b70e77899accc7bcf72ee8f299ef361e1e9d4644
georgestephenson <gf.s@hotmail.com>
2025-06-02 21:25:03 +0100

    Exercise 3.24

---
+++ chapter-3/3-24.scm
@@ -0,0 +1,43 @@
+(define (make-table same-key?)
+  (define (assoc key records)
+    (cond ((null? records) false)
+          ((same-key? key (caar records)) (car records))
+          (else (assoc key (cdr records)))))
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define t1 (make-table equal?))
+((t1 'insert-proc!) 'letters 'a 5)
+;Value: ok
+((t1 'lookup-proc) 'letters 'a)
+;Value: 5
+((t1 'insert-proc!) 'letters 'a 8)
+;Value: ok
+((t1 'lookup-proc) 'letters 'a)
+;Value: 8
