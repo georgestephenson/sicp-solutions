d5e3c04e9ef5ea54330385d8b0a839c43f8049f2
georgestephenson <gf.s@hotmail.com>
2025-07-18 22:52:45 +0100

    Exercise 3.35

---
+++ chapter-3/3-35.scm
@@ -0,0 +1,47 @@
+;; Exercise 3.35
+
+(load "resources/section-3-3-5.scm")
+
+; squarer procedure definition
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0 -- SQUARER" (get-value b))
+            (set-value! a (sqrt (get-value b)) me))
+        (if (has-value? a)
+            (set-value! b (expt (get-value a) 2) me))))
+  (define (process-forget-value) 
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)  
+           (process-new-value))
+          ((eq? request 'I-lost-my-value) 
+           (process-forget-value))
+          (else 
+           (error "Unknown request -- SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+  ; test
+(define A (make-connector))
+(define B (make-connector))
+(squarer A B)
+
+(probe "Square root" A)
+(probe "Square value" B)
+
+(set-value! A 2 'user)
+;Probe: Square root = 2
+Probe: Square value = 4
+
+(forget-value! A 'user)
+;Probe: Square root = ?
+;Probe: Square value = ?
+
+(set-value! B 9 'user)
+;Probe: Square value = 9
+;Probe: Square root = 3
