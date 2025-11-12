bab58c5bd1c58ed2380f1f31baaed7f62938c6f2
georgestephenson <gf.s@hotmail.com>
2025-11-12 22:11:33 +0000

    Exercise 3.50

---
+++ chapter-3/3-50.scm
@@ -0,0 +1,50 @@
+; prerequisites
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+
+; definition
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+; test
+
+(display-stream 
+  (stream-map +
+              (stream-enumerate-interval 1 10)
+              (stream-enumerate-interval 21 30)
+              (stream-enumerate-interval 71 80)))
+; 93
+; 96
+; 99
+; 102
+; 105
+; 108
+; 111
+; 114
+; 117
+; 120
