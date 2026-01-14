777c107e10b2334de079560519858f9fd70e0f2f
georgestephenson <gf.s@hotmail.com>
2026-01-14 19:01:18 +0000

    Exercise 3.66

---
+++ chapter-3/resources/3-66.scm
@@ -0,0 +1,29 @@
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define (stream-take s n)
+  (cond ((or (zero? n) (stream-null? s))
+         '())
+        (else
+         (cons (stream-car s)
+               (stream-take (stream-cdr s) (- n 1))))))
+
+(stream-take (pairs integers integers) 5001)
