efdfdc82711b222e3c8d38e0a6d800113301f7f3
georgestephenson <gf.s@hotmail.com>
2026-01-15 22:55:01 +0000

    Exercise 3.77

---
+++ chapter-3/3-77.scm
@@ -0,0 +1,23 @@
+; exercise 3.77
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (delay (stream-cdr integrand))
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt)))))
+
+; dependencies
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+; test
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
+;Value: 2.716923932235896
